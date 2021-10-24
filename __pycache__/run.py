6a49d7f268813645d1e3603b5d8ce83ed335f0c4
RobbinGIT <robbin.githimbo@student.moringaschool.com>
2021-10-25 01:18:21 +0300

    and create account method ans save account method

--- __pycache__/run.py
+++ __pycache__/run.py
@@ -9,12 +9,12 @@ os.system("clear")
 
 # the user class methods
 
-def create_user(username, password):
+def create_user(user_name, password):
     '''
     this method will create a new account
     '''
     
-    new_user = User(username , password)
+    new_user = User(user_name , password)
     return new_user
 
 def save_user(user):
@@ -22,6 +22,36 @@ def save_user(user):
     method saves the new user account
     '''
     user.save_user()
+    
+def find_user(user_name,password):
+    '''
+    
+    '''
+    return User.finduserbyname(user_name,password)
+
+def check_existing_user(user_name,password):
+    '''
+    
+    '''
+    return User.user_exist(user_name, password)
+
+def refresh():
+    os.system("clear")
+    
+    
+# the class functions
+
+def create_account(myaccount_name, user_name, password):
+    '''
+    create new account
+    '''
+    new_account = Logins(myaccount_name, user_name,password)
+    
+def save_account(credential):
+    '''
+    save new account
+    '''
+    Logins.save_account()
     
 
     
