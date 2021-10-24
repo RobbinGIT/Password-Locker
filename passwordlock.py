b2f05a41da4dc5cfe11b36e055b07bd6ddda00e9
RobbinGIT <robbin.githimbo@student.moringaschool.com>
2021-10-24 21:13:29 +0300

    and methods

--- passwordlock.py
+++ passwordlock.py
@@ -1,10 +1,11 @@
 
 
-
 class User:
     '''
     this user class generates new instances for a user
     '''
+    
+    user_list = []
     
     def __init__(self,username,password):
         '''
@@ -12,3 +13,19 @@ class User:
         '''
         self.username = username
         self.password = password
+        
+    def save_user(self):
+        """ append new user to list"""
+        User.user_list.append(self)
+        
+    @classmethod
+    
+    def user_exist(cls, username, password):
+        '''
+        this methods will check if user actually exiss from the list
+        '''
+        for user in cls.user_list:
+            if user.username == username and user.password == password:
+                return True # this returns a boolean value as the argument
+            
+        return False
--- passwordlock_test.py
+++ passwordlock_test.py
@@ -5,7 +5,7 @@ class TestClass(unittest.TestCase):
     '''
     class to test fuser functions
     '''
-    
+    # Test 1 - check for correct instantion of objects
     def setUp(self):
         '''
         this function will run before anyother individual function
@@ -14,8 +14,30 @@ class TestClass(unittest.TestCase):
         
     def test_init(self):
         '''
-        this method checks if the the instance has been nitilalized
+        this method checks if the the instance has been initilalized properly
         '''
         self.assertEqual(self.new_user.username,'Robbin')
         self.assertEqual(self.new_user.password, "HakunaPassword")
+        
+    # Test 2 - save created account's credentials
+    def test_save_user(self):
+        """
+            test if new user has been saved to the new user_list
+        """
+        self.new_user.save_user()  # add user to list
+        self.assertEqual(len(User.user_list), 1)  # check length of list
+        
+    # test 3 - if object actually exists
+    def test_user_exits(self):
+        """ method to check if user exists in the users list"""
+        self.new_user.save_user()
+        check_user = User("RobbinGit", "HakunaPassword")
+        check_user.save_user()
+        user_exists = User.user_exist("RobbinGit", "HakunaPassword")
+        self.assertTrue(user_exists)
+            
+        
+if __name__ == '__main__':
+    unittest.main()  
+    
     
