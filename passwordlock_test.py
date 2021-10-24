3b9b34095c6cc4d4db000382fa0e9672a1c7ada0
RobbinGIT <robbin.githimbo@student.moringaschool.com>
2021-10-24 20:52:23 +0300

    add setup methods and a method to test if it has been initialized correctly

--- passwordlock_test.py
+++ passwordlock_test.py
@@ -1,2 +1,21 @@
 import unittest
 from passwordlock import User  #importing the class created in the passwordlock.py
+
+class TestClass(unittest.TestCase):
+    '''
+    class to test fuser functions
+    '''
+    
+    def setUp(self):
+        '''
+        this function will run before anyother individual function
+        '''
+        self.new_user = User('Robbin','HakunaPassword')
+        
+    def test_init(self):
+        '''
+        this method checks if the the instance has been nitilalized
+        '''
+        self.assertEqual(self.new_user.username,'Robbin')
+        self.assertEqual(self.new_user.password, "HakunaPassword")
+    
